5666d59b1751110e50f5e352c8fe1cc2fc4ef2e7
AbhayLodhi24 <abhaylodhi24@gmail.com>
2023-07-31 19:07:30 +0530

    Google-oidc Authentication

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oidc');
 
 const app = express();
-
-// console.log(process.env.API_KEY);
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
@@ -29,7 +28,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
 const userSchema = new mongoose.Schema({
     email : String,
-    password : String
+    password : String,
+    googleId : String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -38,12 +38,64 @@ const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, {
+        id: user.id,
+        username: user.username,
+        picture: user.picture
+      });
+    });
+  });
+  
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env['CLIENT_ID'],
+    clientSecret: process.env['CLIENT_SECRET'],
+    callbackURL: 'http://localhost:3000/auth/google/secrets'
+  },
+  function(tokenset, userinfo, done) {
+    console.log(userinfo);
+
+    User.findOne({ googleId : userinfo.id})
+    .then(function(user) {
+        if (!user) {
+          const newUser = new User({
+            googleId: userinfo.id
+          });
+
+          return newUser.save();
+        } else {
+          return user;
+        }
+      })
+      .then(function(user) {
+        done(null, user);
+      })
+      .catch(function(err) {
+        done(err);
+      });
+  }
+));
 
 app.get("/" , function(req , res){
     res.render("home");
 });
+
+app.get("/auth/google" , passport.authenticate("google",{
+    scope: [ 'profile' ]
+  }));
+
+  app.get("/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login"}),
+  function(req, res) {
+    res.redirect("/secrets");
+  });
 
 app.get("/login" , function(req , res){
     res.render("login");
